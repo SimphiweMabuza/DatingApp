de88e8253b79c4b504f989278a6f53fc95ca1f89
SimphiweMabuza <2016363729@ufs4life.ac.za>
2022-04-14 07:26:58 +0200

    End Section12

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.DTOs
 {
     public class UserDto
     {
-        public string username { get; set; }
+        public string Username { get; set; }
         public string Token { get; set; }
         public string PhotoUrl { get; set; }
-        
-
+        public string KnownAs { get; set; }
     }
 }
